3bf025eefd3d93c165dd6617c67fe5f7728240ca
athulya2000 <athulyav00@gmail.com>
2022-12-05 19:58:21 +0530

    administartion login completed

--- src/main/java/com/example/libraryapplication_backend/controller/LibraryController.java
+++ src/main/java/com/example/libraryapplication_backend/controller/LibraryController.java
@@ -1,4 +1,13 @@
 package com.example.libraryapplication_backend.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LibraryController {
+    @PostMapping("/")
+    public String AdministrationLogin(){
+        return "Welcome to administration login page";
+    }
 }
+
